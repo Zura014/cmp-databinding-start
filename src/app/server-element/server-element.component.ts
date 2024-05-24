15f00434cc2db68f977281d5411d7c9423c3b1ab
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-05-24 13:04:17 +0400

    d

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, OnChanges, OnInit, AfterContentInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +6,30 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrl: './server-element.component.css',
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges, AfterContentInit {
   @Input('srvElement') element: {type:string, name:string, content:string};
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+
+
+  constructor() {
+    console.log('constructor called');
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    console.log('text:' + this.header.nativeElement.textContent);
+    console.log('pcontent:' + this.paragraph.nativeElement.textContent);
+  }
+  
+  ngAfterContentInit(): void {
+    console.log('pcontent:' + this.paragraph.nativeElement.textContent);
+  }
 
 }
